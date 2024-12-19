5dd19af4a818f0e195852df2285b39f409121629
Jake <jacob@whatifweb.co.nz>
2024-12-19 18:06:59 +1300

    End of day Thu - Homepage GSAP

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ $(document).ready(function () {
 gsap.registerPlugin(ScrollTrigger)
 gsap.registerPlugin(SplitText) 
 gsap.registerPlugin(DrawSVGPlugin)
+gsap.registerPlugin(MotionPathPlugin)
+gsap.registerPlugin(MorphSVGPlugin)
 
 //morphing shape for the projects section
 var morph1 = gsap.to(".shape-1", { duration: 1, morphSVG:".shape-2", repeat:-1, yoyo:true, repeatDelay:0})
@@ -85,7 +87,9 @@ heroLoad.from(".home-header_lottie.is-slice", {y: -100, x: -200, duration: 0.75,
 //featured projects animation
 var projectsLoad = gsap.timeline({scrollTrigger: {trigger: ".section_projects"}});
 
-projectsLoad.from(".projects_heading-wrapper > h2", {duration: 1, opacity: 0, y: 150, ease: "power3.out"}); 
+var projectsSplit = new SplitText(".projects_heading-wrapper > h2", {type: "lines"});
+
+projectsLoad.from(projectsSplit.lines, {duration: 1, opacity: 0, y: 150, scale: 0.8, ease: "power3.out", stagger: 0.125}); 
 projectsLoad.from(".projects_content > .projects_item > .projects_card", {duration: 1.5, opacity: 0, y:100, ease: "power3.out", stagger: 0.125}, ">-0.5");
 projectsLoad.from(".projects_component > a", {y:100, opacity: 0, ease: "power3.out"}, "<0.25");
 
@@ -95,7 +99,7 @@ projectsLoad.from("#projects-line > svg > path", {drawSVG: 0, duration: 0.6, eas
 //solutions/services section animation
 var servicesLoad = gsap.timeline({scrollTrigger: {trigger: ".section_services"}});
 
-servicesLoad.from(".services_content-top > h2", {y: 200, opacity: 0, duration: 1, ease: "power3.out"});
+servicesLoad.from(".services_content-top > h2", {y: 200, scale: 0.8, opacity: 0, duration: 1, ease: "power3.out"});
 servicesLoad.from(".services_service-list > .services_service-item", {y: 100, opacity: 0, duration: .5, stagger: 0.25, ease: "power.4out"}, "<0.5");
 
 //cta form animation
@@ -106,23 +110,28 @@ ctaFormLoad.to(".cta_graphic", {y: -300, opacity: 0, ease: "power3.out"}, ">+0.2
 ctaFormLoad.to(".cta_form", {gap: 400, y: -300}, "<.05").from("#cta-form-bottom", {y: 800, ease: "power3.out"}, "<0.25");
 
 //process section animation
-var processLoad = gsap.timeline({scrollTrigger: {trigger: ".process_component", scrub: true, start: "top bottom", end: "bottom bottom", markers: true}});
+var processLoad = gsap.timeline({scrollTrigger: {trigger: ".process_component", scrub: true, start: "top bottom", end: "bottom bottom", markers: false}});
 
 processLoad.to("#process-heading", {y: -700, ease: "power3.out"}, .5);
-processLoad.to(".process_critter", {width: "100dvw", height: "50dvw", ease: "power4.out"}, ">");
-// processLoad.to(".process_card-wrapper", {x: "-300dvw"}, ">-0.1");
-processLoad.to(".process_card-wrapper > .card", {x: "-100dvw", stagger: 0.25, motionPath: {Path: "#process-path", align: "#process-path"}}, ">-0.1");
+processLoad.to(".process_critter", {width: "100dvw", height: "50dvw", ease: "power4.out"}, ">-0.5");
+gsap.set(".process_card-wrapper > .card", {xPercent: -50, yPercent: -50, transformOrigin: "50%, 50%"});
+processLoad.to(".process_card-wrapper > .card", {stagger: 0.08, motionPath: {path: "#process-path", align: "#process-path", autoRotate: 180}}, ">");
 
 //testimonials section animation
 var testimonialsLoad = gsap.timeline({scrollTrigger: {trigger: ".section_testimonials"}});
 
-testimonialsLoad.from(".testimonials_star-wrapper > div", {opacity: 0, y: 75, duration: 0.75, ease: "power3.out"}).from("#testimonial-heading", {opacity: 0, y: 100, duration: 1, ease: "power4.out"}, "<0.25");
+var testimonialSplit = new SplitText("#testimonial-heading", {type: "lines"});
+
+testimonialsLoad.from(".testimonials_star-wrapper > div", {opacity: 0, y: 75, duration: 0.75, ease: "power3.out"}).from(testimonialSplit.lines, {opacity: 0, y: 100, duration: 1, scale: 0.8, stagger: 0.125, ease: "power4.out"}, "<0.25");
 
 //About Section Animation
 var aboutLoad = gsap.timeline({scrollTrigger: {trigger: ".section_home-about"}});
 
+var aboutSplit = new SplitText(".home-about_content-left > h2", {type: "lines"});
+
 //text
-aboutLoad.from(".home-about_content-left > p, .home-about_content-left > h2, .home-about_content-left > a", {y:100, duration: 1, ease:"power3.out", opacity: 0, stagger: 0.125});
+aboutLoad.from(aboutSplit.lines, {y: 100, scale: 0.8, opacity: 0, stagger: 0.125, ease: "power3.out"}, 0);
+aboutLoad.from(".home-about_content-left > p, .home-about_content-left > a", {duration: 1, ease:"power3.out", opacity: 0, stagger: 0.125}, "<0.5");
 
 //images
 aboutLoad.from(".home-about_background-shape", {scale: 0}, -.25);
@@ -140,14 +149,25 @@ ctaLoad.from(".quote-cta_component", {scale: 0.7, y: 400, duration: 1.75, ease:"
 
 //text span
 ctaLoad.from(".quote-cta_heading > .text-color-alternate", {color: "#fff9f3", duration: 0.5, ease: "power3.out"}, 0.75).to(".text-color-alternate", {scale: 1.2, duration: 0.25, ease: "power4.out"}).to(".text-color-alternate", {scale: 1, duration: 0.25,})
-.from("#ctaScribble", {drawSVG: 0, duration: 1, ease: "power4.in"}, "<0.25");
+.from("#ctaScribble", {drawSVG: 0, duration: 1, ease: "power4.in"}, "<-0.25");
 
 //content
-ctaLoad.fromTo(".quote-cta_heading-wrapper", {y:100}, {y: 0, duration: 1, ease: "power2.out"}, "<1");
+ctaLoad.fromTo(".quote-cta_heading-wrapper", {y:100}, {y: 0, duration: 1, ease: "power2.out"}, "<");
 ctaLoad.from(".quote-cta_subheading", {y:150, duration: 0.875, ease: "power2.out", opacity: 0}, "<0.5");
 ctaLoad.from("#ctaButton", {y: 200, duration: 0.875, ease: "power2.out", opacity: 0,}, "<0.125");
 
 //blog section
 var blogLoad = gsap.timeline({scrollTrigger: {trigger: ".section_related-blog"}});
 
-blogLoad.from(".related-blog_heading-wrapper > div", {y: 100, opacity: 0, duration: 1, ease: "power3.out"}).from(".related-blog_list > .related-blog_item", {y:180, opacity: 0, duration: 1, ease: "power4.out", stagger: 0.125}, "<0.5");
+blogLoad.from(".related-blog_heading-wrapper > div", {y: 100, scale: 0.8, opacity: 0, duration: 1, ease: "power3.out"}).from(".related-blog_list > .related-blog_item", {y:180, opacity: 0, duration: 1, ease: "power4.out", stagger: 0.125}, "<0.5");
+
+//footer animation
+var footerLoad = gsap.timeline({scrollTrigger: {trigger: ".footer_component"}});
+
+var footerSplit = new SplitText(".footer_top-wrapper > h2", {type: "lines"});
+
+footerLoad.from(footerSplit.lines, {scale: 0.8, opacity: 0, y: 100, stagger: 0.125, ease: "power3.out", duration: 1}, 0);
+footerLoad.from(".footer_top-wrapper > p", {scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1}, "<0.25");
+footerLoad.from(".footer_form_component", {scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1}, "<0.125");
+footerLoad.from(".footer_links-wrapper > a", {opacity: 0, stagger: 0.125, ease: "power4.out", duration: 0.25}, "<0.5");
+footerLoad.from(".footer_contact-wrapper > .footer_contact-item", {y: 50, opacity: 0, stagger: 0.125, ease: "power4.out", duration: 1}, "<0.5");
