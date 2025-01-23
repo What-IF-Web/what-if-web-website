49482aea530dbcf9d3dad740879c3aedcf882b3b
Jake <jacob@whatifweb.co.nz>
2025-01-23 15:36:03 +1300

    brought back case and reassigned urls

--- app.js
+++ app.js
@@ -16,28 +16,40 @@ const homeScriptURL = new URL(
   'https://what-if-web.github.io/what-if-web-website/home.js'
 );
 const caseStudiesScriptURL = new URL(
-  'https://what-if-web.github.io/what-if-web-website/home.js'
+  'https://what-if-web.github.io/what-if-web-website/case-studies.js'
 );
 const caseStudiesTempScriptURL = new URL(
-  'https://what-if-web.github.io/what-if-web-website/home.js'
+  'https://what-if-web.github.io/what-if-web-website/case-studies-template.js'
 );
 const contactScriptURL = new URL(
-  'https://what-if-web.github.io/what-if-web-website/home.js'
+  'https://what-if-web.github.io/what-if-web-website/contact.js'
 );
 const notFoundScriptURL = new URL(
-  'https://what-if-web.github.io/what-if-web-website/home.js'
+  'https://what-if-web.github.io/what-if-web-website/not-found.js'
 );
 const pricingScriptURL = new URL(
-  'https://what-if-web.github.io/what-if-web-website/home.js'
+  'https://what-if-web.github.io/what-if-web-website/pricing.js'
 );
 
-
-import(homeScriptURL.href)
-import(caseStudiesScriptURL.href)
-import(caseStudiesTempScriptURL.href)
-import(contactScriptURL.href)
-import(notFoundScriptURL.href)
-import(pricingScriptURL.href)
+switch (true) {
+  case homeHeader !== null:
+    import(homeScriptURL.href)
+    break;
+  case caseStudy !== null:
+  import(caseStudiesTempScriptURL.href)
+    break;
+  case caseStudies !== null:
+    import(caseStudiesScriptURL.href)
+    break;
+  case notFound !== null:
+    import(notFoundScriptURL.href)
+    break;
+  case url.includes("pricing"):
+    import(pricingScriptURL.href)
+    break;
+  case url.includes("contact"):
+    import(contactScriptURL.href)
+}
 
 /* testimonial slider */
 $(document).ready(function () {
