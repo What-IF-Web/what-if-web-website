07795c04033a61e9da0f23943c560d019d81f421
Jake <jacob@whatifweb.co.nz>
2025-01-23 14:21:12 +1300

    absolute url with case test

--- app.js
+++ app.js
@@ -30,85 +30,53 @@ const pricingScriptURL = new URL(
   'https://what-if-web.github.io/what-if-web-website/home.js'
 );
 
-import(homeScriptURL.href)
-  .then((module) => {
-    module.default();
-  }
-);
-import(caseStudiesScriptURL.href)
-  .then((module) => {
-    module.default();
-  }
-);
-import(caseStudiesTempScriptURL.href)
-  .then((module) => {
-    module.default();
-  }
-);
-import(contactScriptURL.href)
-  .then((module) => {
-    module.default();
-  }
-);
-import(notFoundScriptURL.href)
-  .then((module) => {
-    module.default();
-  }
-);
-import(pricingScriptURL.href)
-  .then((module) => {
-    module.default();
-  }
-);
 
-
-/*switch (true) {
+switch (true) {
   case homeHeader !== null:
-    import("https://what-if-web.github.io/what-if-web-website/home.js").then(
-      (module) => {
-        module.default();
-      }
+    import(homeScriptURL.href)
+    .then((module) => {
+      module.default();
+    }
     );
     break;
   case caseStudy !== null:
-    import(
-      "https://what-if-web.github.io/what-if-web-website/case-studies-template.js"
-    ).then((module) => {
+  import(caseStudiesTempScriptURL.href)
+    .then((module) => {
       module.default();
-    });
+    }
+    );
     break;
   case caseStudies !== null:
-    import(
-      "https://what-if-web.github.io/what-if-web-website/case-studies.js"
-    ).then((module) => {
+    import(caseStudiesScriptURL.href)
+    .then((module) => {
       module.default();
-    });
+    }
+    );  
     break;
   case notFound !== null:
-    import(
-      "https://what-if-web.github.io/what-if-web-website/not-found.js"
-    ).then((module) => {
+    import(notFoundScriptURL.href)
+    .then((module) => {
       module.default();
-    });
-    console.log("not-found");
+    }
+    );
     break;
   case url.includes("pricing"):
-    import("https://what-if-web.github.io/what-if-web-website/pricing.js").then(
-      (module) => {
-        module.default();
-      }
+    import(pricingScriptURL.href)
+    .then((module) => {
+      module.default();
+    }
     );
     break;
   case url.includes("contact"):
-    import("https://what-if-web.github.io/what-if-web-website/contact.js").then(
-      (module) => {
-        module.default();
-      }
+    import(contactScriptURL.href)
+    .then((module) => {
+      module.default();
+    }
     );
     break;
   default:
     console.log("no-script");
-}*/
+}
 
 /* testimonial slider */
 $(document).ready(function () {
