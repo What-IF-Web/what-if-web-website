dae470b59c600613e104e23b95b114a3e9bd9ea4
Jake <jacob@whatifweb.co.nz>
2025-02-18 12:16:43 +1300

    commit idk

--- test.js
+++ test.js
@@ -1,38 +1,18 @@
 //case studies card testing
 console.log("test-active");
 
-//case studies cards animation
-// const cards = document.querySelectorAll(".case-studies_item");
-
-// var cardsTl = gsap.timeline();
-
-// cards.forEach((card, index) => {
-//   cardsTl.from(card, {
-//     scrollTrigger: {
-//       trigger: card,
-//       start: "top bottom",
-//       end: "top 50%",
-//       scrub: true,
-//       markers: false
-//     },
-//     scale: 0.8,
-//     y: 100,
-//     ease: "power3.out",
-//     rotateX: 5,
-//     opacity: 0
-//   });
-// });
-
-// $(".case-studies_checkbox-button").click(cardsTl.ScrollTrigger.refresh());
-
+//Morph SVG
 gsap.registerPlugin(MorphSVGPlugin);
 
-var morphPath = gsap.timeline({repeat: -1});
+var morphPath = gsap.timeline({ repeat: -1 });
 
-morphPath.to(".path1", { duration: 1, morphSVG:{shape: ".path2", type: "rotational"}, duration: 2})
-// morphPath.to(".path1", { duration: 1, morphSVG:{shape: ".path3", type: "rotational"}, duration: 2})
-// morphPath.to(".path1", { duration: 1, morphSVG:{shape: ".path4", type: "rotational"}, duration: 2})
-// morphPath.to(".path1", { duration: 1, morphSVG:{shape: ".path5", type: "rotational"}, duration: 2})
-// morphPath.to(".path1", { duration: 1, morphSVG:{shape: ".path6", type: "rotational"}, duration: 2})
-// morphPath.to(".path1", { duration: 1, morphSVG:{shape: ".path7", type: "rotational"}, duration: 2})
-// morphPath.to(".path1", { duration: 1, morphSVG:{shape: ".path1", type: "rotational"}, duration: 2})
+morphPath
+  .to(".path1", { duration: 2, morphSVG: { shape: ".path2", type: "rotational" } })
+  .to(".path1", { duration: 2, morphSVG: { shape: ".path3", type: "rotational" } })
+  .to(".path1", { duration: 2, morphSVG: { shape: ".path4", type: "rotational" } })
+  .to(".path1", { duration: 2, morphSVG: { shape: ".path5", type: "rotational" } })
+  .to(".path1", { duration: 2, morphSVG: { shape: ".path6", type: "rotational" } })
+  .to(".path1", { duration: 2, morphSVG: { shape: ".path7", type: "rotational" } })
+  .to(".path1", { duration: 2, morphSVG: { shape: ".path1", type: "rotational" } });
+
+  console.log("after-morph");
