52c58125a7dcb456dc0cd8d61b3c603e5917f32c
Jake <jacob@whatifweb.co.nz>
2025-02-21 10:54:59 +1300

    changed projects load speed

--- home.js
+++ home.js
@@ -125,8 +125,8 @@ projectsLoad.from(projectsSplit.lines, {
 });
 projectsLoad.from(
   ".projects_content > .projects_item > .projects_card",
-  { duration: 1, opacity: 0, y: 100, ease: "power3.out", stagger: 0.125 },
-  ">-0.5"
+  { duration: 0.75, opacity: 0, y: 60, ease: "power3.out", stagger: 0.1 },
+  ">-0.75"
 );
 projectsLoad.from(
   ".projects_component > a",
