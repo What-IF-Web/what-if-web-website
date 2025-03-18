459d6af45cf48308993d9a73f156f6e308acf70d
TeManihera <temanihera.gay@hotmail.co.nz>
2025-03-19 12:37:04 +1300

    Get rid of import

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ ScrollSmoother.create({
   effects: true,
 });
 
-import "./about";
+/*this is where you add imports for localhost */
+// import "./about";
 
 const url = window.location.pathname;
 const scriptsMap = new Map([
