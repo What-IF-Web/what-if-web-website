079c0c95190bb32b8cf22587c228ff58e3725b37
Jake <jacob@whatifweb.co.nz>
2025-03-28 18:06:04 +1300

    changed footer and filters animations

--- resources.js
+++ resources.js
@@ -4,7 +4,7 @@ var resourceHeaderLoad = gsap.timeline({scrollTrigger: {trigger: ".section_resou
 
 resourceHeaderLoad.from(".resources_top-content > h1", {y: 150, duration: .75, opacity: 0, ease: "power3.out"});
 resourceHeaderLoad.from(".resources_top-content > p", {y: 100, duration: 1, opacity: 0, ease: "power3.out"}, "<0.125");
-resourceHeaderLoad.from(".resources_tab-link", {y: 120, duration: 0.75, stagger: 0.125, ease: "power3.out"}, "<0.5");
+resourceHeaderLoad.from(".resources_tab-link", {y: 35, duration: 0.75, stagger: 0.125, ease: "power3.out"}, "<0.5");
 
 //simplified cards animation
 const cards = document.querySelectorAll(".resources_item");
@@ -20,5 +20,5 @@ var footerSplit = new SplitText(".footer_top-wrapper > h2", {type: "lines"});
 footerLoad.from(footerSplit.lines, {scale: 0.8, opacity: 0, y: 100, stagger: 0.125, ease: "power3.out", duration: 1}, 0);
 footerLoad.from(".footer_top-wrapper > p", {scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1}, "<0.25");
 footerLoad.from(".footer_form_component", {scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1}, "<0.125");
-footerLoad.from(".footer_links-wrapper > a", {opacity: 0, stagger: 0.125, ease: "power4.out", duration: 0.25}, "<0.5");
+footerLoad.from(".footer_links-wrapper > a", {y: 32, opacity: 0, stagger: 0.125, ease: "power4.out", duration: 0.25}, "<0.5");
 footerLoad.from(".footer_contact-wrapper > .footer_contact-item", {y: 50, opacity: 0, stagger: 0.125, ease: "power4.out", duration: 1}, "<0.5");
