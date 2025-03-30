3f366f709c5d49760163888a2bc09ca31f5667a1
Jake <jacob@whatifweb.co.nz>
2025-03-31 11:31:51 +1300

    updated footer animation

--- not-found.js
+++ not-found.js
@@ -14,5 +14,5 @@ var footerSplit = new SplitText(".footer_top-wrapper > h2", {type: "lines"});
 footerLoad.from(footerSplit.lines, {scale: 0.8, opacity: 0, y: 100, stagger: 0.125, ease: "power3.out", duration: 1}, 0);
 footerLoad.from(".footer_top-wrapper > p", {scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1}, "<0.25");
 footerLoad.from(".footer_form_component", {scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1}, "<0.125");
-footerLoad.from(".footer_links-wrapper > a", {y: 32, opacity: 0, stagger: 0.125, ease: "power4.out", duration: 0.25}, "<0.5");
+footerLoad.from(".footer_links-wrapper > a", {opacity: 0, stagger: 0.125, ease: "power4.out", duration: 0.5}, "<0.5");
 footerLoad.from(".footer_contact-wrapper > .footer_contact-item", {y: 50, opacity: 0, stagger: 0.125, ease: "power4.out", duration: 1}, "<0.5");
