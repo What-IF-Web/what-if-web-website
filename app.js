c3d942a0fdb66b32ad24e15435345bf6ccfa09cc
Jake <jacob@whatifweb.co.nz>
2025-04-03 18:11:23 +1300

    QA fixes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ ScrollSmoother.create({
 });
 
 /*this is where you add imports for localhost */
-// import "./blog-template"
+// import "./home"
 
 const url = window.location.pathname;
 const scriptsMap = new Map([
