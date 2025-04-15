0d183e28757d1a2f19ea2e8c5da2f5073feb8c6c
Jake <jacob@whatifweb.co.nz>
2025-04-16 10:54:08 +1200

    created modular functions

--- about.js
+++ about.js
@@ -1,4 +1,11 @@
+import "./app"
+
 //about page scroll section
+
+gsap.defaults({
+  ease: "power4.out",
+  duration: 1,
+});
 
 let am = gsap.matchMedia();
 
@@ -26,49 +33,60 @@ var aboutSplit = new SplitText(".about-header_heading-wrapper > h1", {
   type: "lines",
 });
 
-aboutHero.from(
-  aboutSplit.lines,
-  { opacity: 0, y: 100, ease: "power4.out", duration: 1.5, stagger: 0.125 },
-  "0"
-);
+aboutHero.from(aboutSplit.lines, { opacity: 0, y: 100, stagger: 0.125 }, 0);
 aboutHero.from(
   ".home-about_card-wrapper.is-about-2",
-  { opacity: 0, y: 700, x: 400, rotation: "50", ease: "power3.out", duration: 1},
-  "0.5"
+  { opacity: 0, y: 500, x: 400, rotation: "50" },
+  0.5
 );
 aboutHero.from(
   ".home-about_card-wrapper.is-about-1",
-  { opacity: 0, y: 600, x: 300, rotation: "40", ease: "power3.out", duration: 1},
-  "0.75"
+  { opacity: 0, y: 300, x: 300, rotation: "40" },
+  0.75
 );
 
-var aboutHeroLast = gsap.timeline({scrollTrigger: {trigger: ".about-header_trigger"}});
+var aboutHeroLast = gsap.timeline({
+  scrollTrigger: { trigger: ".about-header_trigger" },
+});
 
-aboutHeroLast.from(".about-header_minimal-card.is-first", {opacity: 0, y: -900, x: -150, rotation: "50", ease: "power3.out", duration: 1}, 0);
-aboutHeroLast.from(".about-header_minimal-card.is-second", {opacity: 0, y: -1000, x: -300, rotation: "64", ease: "power3.out", duration: 1}, 0.125);
-aboutHeroLast.from(".about-header_minimal-card.is-last", {opacity: 0, y: -1200, x: -50, rotation: "80", ease: "power3.out", duration: 1}, 0.25);
-
-aboutHeroLast.from(".about-header_subheading", {y: 100, opacity: 0, duration: 1, ease: 'power3.out'}, 0.25);
-aboutHeroLast.from(".about-header_text", {y: 100, opacity: 0, duration: 1, ease: 'power3.out'}, 0.5);
-aboutHeroLast.from(".about-header_paragraph-wrapper", {y: 100, opacity: 0, duration: 1, ease: 'power3.out'}, 0.75);
-aboutHeroLast.from(".about-header_card-ghost", {y: 100, scale: 0.8, opacity: 0, duration: 1, ease: 'power3.out'}, 0.5);
-aboutHeroLast.from(".about-header_card-brazzo", {y: 80, scale: 0.8, opacity: 0, duration: 1, ease: 'power3.out'}, 0.75);
+aboutHeroLast.from(
+  ".about-header_minimal-card.is-first",
+  { opacity: 0, y: -700, x: -150, rotation: "50" },
+  0
+);
+aboutHeroLast.from(
+  ".about-header_minimal-card.is-second",
+  { opacity: 0, y: -800, x: -300, rotation: "64" },
+  0.125
+);
+aboutHeroLast.from(
+  ".about-header_minimal-card.is-last",
+  { opacity: 0, y: -900, x: -50, rotation: "80" },
+  0.25
+);
+aboutHeroLast.from(
+  ".about-header_heading-wrapper > div",
+  { y: 100, opacity: 0, stagger: 0.25 },
+  0.25
+);
+aboutHeroLast.from(
+  ".about-header_card-ghost",
+  { y: 100, scale: 0.8, opacity: 0 },
+  0.5
+);
+aboutHeroLast.from(
+  ".about-header_card-brazzo",
+  { y: 80, scale: 0.8, opacity: 0 },
+  0.75
+);
 
 //team load
-var teamLoad = gsap.timeline({ scrollTrigger: { trigger: ".section_team" } });
+var teamLoad = gsap.timeline({ scrollTrigger: { trigger: ".section_team"} });
 
-teamLoad.from(
-  ".team_content-top",
-  { y: 75, opacity: 0, duration: 1, ease: "power4.out" },
-  "0"
-);
-teamLoad.from(
-  ".team_item",
-  { y: 150, opacity: 0, stagger: 0.125, duration: 1, ease: "power4.out" },
-  "0.25"
-);
+teamLoad.from(".team_content-top", { y: 75, opacity: 0 }, 0);
+teamLoad.from(".team_item", { y: 150, opacity: 0, stagger: 0.125 }, 0.25);
 
-//testimonials section animation
+//testimonials section animation -- need to modularise
 var testimonialsLoad = gsap.timeline({
   scrollTrigger: { trigger: ".section_testimonials", start: "-=400 center" },
 });
@@ -95,7 +113,7 @@ testimonialsLoad
     "<0.25"
   );
 
-//footer animation
+//footer animation -- need to modularise
 var footerLoad = gsap.timeline({
   scrollTrigger: { trigger: ".footer_component", start: "-=400 center" },
 });
