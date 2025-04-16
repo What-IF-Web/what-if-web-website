17a2380e78c789eabfb65ece36a8908d5b43f385
TeManihera <temanihera.gay@hotmail.co.nz>
2025-04-16 12:35:55 +1200

    Removed unwanted code from about.js

--- about.js
+++ about.js
@@ -79,74 +79,74 @@ aboutHeroLast.from(
 );
 
 //team load
-var teamLoad = gsap.timeline({ scrollTrigger: { trigger: ".section_team"} });
+var teamLoad = gsap.timeline({ scrollTrigger: { trigger: ".section_team" } });
 
 teamLoad.from(".team_content-top", { y: 75, opacity: 0 }, 0);
 teamLoad.from(".team_item", { y: 150, opacity: 0, stagger: 0.125 }, 0.25);
 
-//testimonials section animation -- need to modularise
-var testimonialsLoad = gsap.timeline({
-  scrollTrigger: { trigger: ".section_testimonials", start: "-=400 center" },
-});
+// //testimonials section animation -- need to modularise
+// var testimonialsLoad = gsap.timeline({
+//   scrollTrigger: { trigger: ".section_testimonials", start: "-=400 center" },
+// });
 
-var testimonialSplit = new SplitText("#testimonial-heading", { type: "lines" });
+// var testimonialSplit = new SplitText("#testimonial-heading", { type: "lines" });
 
-testimonialsLoad
-  .from(".testimonials_star-wrapper > div", {
-    opacity: 0,
-    y: 75,
-    duration: 0.75,
-    ease: "power3.out",
-  })
-  .from(
-    testimonialSplit.lines,
-    {
-      opacity: 0,
-      y: 100,
-      duration: 1,
-      scale: 0.8,
-      stagger: 0.125,
-      ease: "power4.out",
-    },
-    "<0.25"
-  );
+// testimonialsLoad
+//   .from(".testimonials_star-wrapper > div", {
+//     opacity: 0,
+//     y: 75,
+//     duration: 0.75,
+//     ease: "power3.out",
+//   })
+//   .from(
+//     testimonialSplit.lines,
+//     {
+//       opacity: 0,
+//       y: 100,
+//       duration: 1,
+//       scale: 0.8,
+//       stagger: 0.125,
+//       ease: "power4.out",
+//     },
+//     "<0.25"
+//   );
 
-//footer animation -- need to modularise
-var footerLoad = gsap.timeline({
-  scrollTrigger: { trigger: ".footer_component", start: "-=400 center" },
-});
+// //footer animation -- need to modularise
+// var footerLoad = gsap.timeline({
+//   scrollTrigger: { trigger: ".footer_component", start: "-=400 center" },
+// });
 
-var footerSplit = new SplitText(".footer_top-wrapper > h2", { type: "lines" });
+// var footerSplit = new SplitText(".footer_top-wrapper > h2", { type: "lines" });
 
-footerLoad.from(
-  footerSplit.lines,
-  {
-    scale: 0.8,
-    opacity: 0,
-    y: 100,
-    stagger: 0.125,
-    ease: "power3.out",
-    duration: 1,
-  },
-  0
-);
-footerLoad.from(
-  ".footer_top-wrapper > p",
-  { scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1 },
-  "<0.25"
-);
-footerLoad.from(
-  ".footer_form_component",
-  { scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1 },
-  "<0.125"
-);
-footerLoad.from(
-  ".footer_links-wrapper > a",
-  { opacity: 0, stagger: 0.125, ease: "power4.out", duration: 0.5 },
-  "<0.5"
-);
-footerLoad.from(
-  ".footer_contact-wrapper > .footer_contact-item",
-  { y: 50, opacity: 0, stagger: 0.125, ease: "power4.out", duration: 1 },
-  "<0.5"
-);
+// footerLoad.from(
+//   footerSplit.lines,
+//   {
+//     scale: 0.8,
+//     opacity: 0,
+//     y: 100,
+//     stagger: 0.125,
+//     ease: "power3.out",
+//     duration: 1,
+//   },
+//   0
+// );
+// footerLoad.from(
+//   ".footer_top-wrapper > p",
+//   { scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1 },
+//   "<0.25"
+// );
+// footerLoad.from(
+//   ".footer_form_component",
+//   { scale: 0.8, opacity: 0, y: 100, ease: "power3.out", duration: 1 },
+//   "<0.125"
+// );
+// footerLoad.from(
+//   ".footer_links-wrapper > a",
+//   { opacity: 0, stagger: 0.125, ease: "power4.out", duration: 0.5 },
+//   "<0.5"
+// );
+// footerLoad.from(
+//   ".footer_contact-wrapper > .footer_contact-item",
+//   { y: 50, opacity: 0, stagger: 0.125, ease: "power4.out", duration: 1 },
+//   "<0.5"
+// );
