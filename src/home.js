ec8356147c6bc26be9c2cbccb2bc485ef42ce64b
TeManihera <temanihera.gay@hotmail.co.nz>
2025-05-02 12:19:19 +1200

    Adjusted home code.

--- src/home.js
+++ src/home.js
@@ -1,309 +1,310 @@
-//home page stuff
-
-gsap.defaults({
+// Default animation settings
+const animationDefaults = {
   ease: "power4.out",
   duration: 1,
+};
+
+// Reusable ScrollTrigger configuration
+const defaultScrollTrigger = (trigger, start = "top center", options = {}) => ({
+  trigger,
+  start,
+  toggleActions: "play none none reset",
+  ...options,
 });
 
-//hero section animation
-var heroSplit = new SplitText(".home-header_heading", { type: "lines" });
+// Reusable SVG animation function
+const animateSVG = (selector, timeline, offset = 0) => {
+  timeline
+    .from(`${selector} > svg > path`, { drawSVG: "0% 0%" }, offset)
+    .to(`${selector} > svg > path`, { drawSVG: "100% 100%" }, ">-0.3")
+    .to(`${selector} > svg > path`, { opacity: 0, duration: 0 }, ">-0.05");
+};
 
-var heroLoad = gsap.timeline({
-  scrollTrigger: { trigger: ".section_home-header" },
-});
+// Selectors for repeated elements
+const selectors = {
+  heroHeading: ".home-header_heading",
+  heroSubheading: ".home-header_subheading",
+  heroCta: ".home-header_component > a",
+  lottieDinkus: ".home-header_lottie.is-dinkus",
+  lottieBrazzo: ".home-header_lottie.is-brazzo",
+  lottieSlice: ".home-header_lottie.is-slice",
+  aboutHeading: ".home-about_heading-wrapper > h2",
+  aboutContent: ".home-about_content-left > p, .home-about_content-left > a",
+  aboutBackground: ".home-about_background-shape",
+  aboutCardFirst: ".home-about_card-wrapper.is-first",
+  aboutCardSecond: ".home-about_card-wrapper.is-second",
+  aboutCardThird: ".home-about_card-wrapper.is-third",
+  aboutLottie: ".home-about_lottie",
+  aboutLottie2: ".home-about_lottie-2",
+  ctaForm: ".cta_form",
+  ctaHeadingSpan: ".cta_heading-span",
+  ctaFormInput: ".cta_form_input-wrapper",
+  ctaFormBottom: "#cta-form-bottom",
+  ctaFormOrange: "#cta-form-orange",
+  ctaBurstLines: ".cta_burst-lines",
+  ctaScribble: ".cta_scribble",
+  processComponent: ".process_component",
+  processHeadingSpan: ".process_heading-span",
+  processCritterWrapper: ".process_critter-wrapper",
+  processCritter: ".process_critter",
+  processCritterEyes: ".process_critter-eyes",
+  processCard: ".process_card-wrapper > .process_card",
+  processPath: "#process-path",
+  servicesSection: ".section_services",
+  servicesContentTop: ".services_content-top-wrapper",
+};
 
-heroLoad
-  .from(heroSplit.lines, {
-    opacity: 0,
-    y: 400,
-    stagger: 0.125,
-  })
-  .from(
-    ".home-header_heading",
-    { scale: 0.6, duration: 0.625, ease: "back.out(1.7)" },
-    ">-0.5"
-  );
-heroLoad.from(".home-header_subheading", { y: 200, opacity: 0 }, ">-0.25");
-heroLoad.from(".home-header_component > a", { y: 200, opacity: 0 }, ">-0.25");
-heroLoad.from(
-  ".home-header_lottie.is-dinkus",
-  { y: 70, x: 180, duration: 0.5, scale: 0 },
-  ">-0.5"
-);
-heroLoad.from(
-  ".home-header_lottie.is-brazzo",
-  { y: -200, x: 240, duration: 0.625, scale: 0 },
-  "<-0.25"
-);
-heroLoad.from(
-  ".home-header_lottie.is-slice",
-  { y: -100, x: -200, duration: 0.75, scale: 0 },
-  "<-0.125"
-);
-
-//About Section Animation
-var aboutLoad = gsap.timeline({
-  scrollTrigger: { trigger: ".section_home-about", start: "top center" },
-});
-
-var aboutSplit = new SplitText(".home-about_heading-wrapper > h2", {
-  type: "lines",
-});
-
-aboutLoad.from(
-  aboutSplit.lines,
-  { y: 100, scale: 0.8, opacity: 0, stagger: 0.125 },
-  0
-);
-aboutLoad.from(
-  ".home-about_content-left > p, .home-about_content-left > a",
-  { opacity: 0, stagger: 0.125 },
-  "<0.5"
-);
-aboutLoad.from(".home-about_background-shape", { scale: 0 }, -0.25);
-aboutLoad.from(
-  ".home-about_card-wrapper.is-first",
-  { x: 2500, rotation: 100 },
-  "<0.5"
-);
-aboutLoad.from(
-  ".home-about_card-wrapper.is-second",
-  { x: 2500, rotation: 60 },
-  0.25
-);
-aboutLoad.from(
-  ".home-about_card-wrapper.is-third",
-  { x: 2000, rotation: 130 },
-  0.125
-);
-aboutLoad
-  .from(".home-about_lottie", { scale: 0 }, ">0.125")
-  .from(".home-about_lottie-2", { scale: 0 }, "<0.25");
-
-let mm = gsap.matchMedia();
-
-// desktop
-mm.add("(min-width: 991px)", () => {
-  var ctaFormTrigger = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".cta_form",
-      start: "-=400 center",
-    },
+// Hero Section Animation
+const animateHero = () => {
+  const heroSplit = new SplitText(selectors.heroHeading, { type: "lines" });
+  const heroLoad = gsap.timeline({
+    scrollTrigger: defaultScrollTrigger(".section_home-header"),
+    defaults: animationDefaults,
   });
-  var ctaFormLoad = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".cta_component",
-      scrub: true,
-      start: "top top",
-      end: "bottom bottom",
-      pin: ".cta_form-block",
-      pinSpacing: false,
-    },
+
+  heroLoad
+    .from(heroSplit.lines, { opacity: 0, y: 400, stagger: 0.125 })
+    .from(
+      selectors.heroHeading,
+      { scale: 0.6, duration: 0.625, ease: "back.out(1.7)" },
+      ">-0.5"
+    )
+    .from(
+      [selectors.heroSubheading, selectors.heroCta],
+      { y: 200, opacity: 0, stagger: 0.125 },
+      ">-0.25"
+    )
+    .from(
+      [selectors.lottieDinkus, selectors.lottieBrazzo, selectors.lottieSlice],
+      {
+        y: (i) => [70, -200, -100][i],
+        x: (i) => [180, 240, -200][i],
+        scale: 0,
+        duration: 0.625,
+        stagger: 0.125,
+      },
+      ">-0.5"
+    );
+};
+
+// About Section Animation
+const animateAbout = () => {
+  const aboutSplit = new SplitText(selectors.aboutHeading, { type: "lines" });
+  const aboutLoad = gsap.timeline({
+    scrollTrigger: defaultScrollTrigger(selectors.aboutHeading, "top center"),
+    defaults: animationDefaults,
   });
-  ctaFormLoad.from(
-    ".cta_heading-span",
-    {
-      y: 200,
-      scale: 0.95,
-      opacity: 0,
-      stagger: 0.05,
-    },
-    0
-  );
-  ctaFormLoad.from(".cta_form_input-wrapper", { y: 300, opacity: 0 }, "<");
-  ctaFormLoad.from("#cta-form-bottom", { y: 400, opacity: 0 }, "<");
-  ctaFormLoad.to(
-    ".cta_heading-span",
-    {
-      y: -400,
-      stagger: 0.05,
-      scale: 0.95,
-      opacity: 0,
-    },
-    ">+0.25"
-  );
-  ctaFormLoad.to(".cta_form_input-wrapper", { y: -300, opacity: 0 }, "<+.05");
-  ctaFormLoad.to("#cta-form-bottom", { y: -200, opacity: 0 }, "<");
 
-  ctaFormTrigger.from(
-    "#cta-form-orange",
-    { color: "#0f2756", duration: 0.125 },
-    "<.5"
-  );
-  ctaFormTrigger.to(
-    "#cta-form-orange",
-    { scale: 1.1, repeat: 1, yoyo: true, ease: "power2.inOut", duration: 0.3 },
-    "<"
-  );
-  ctaFormTrigger.from(
-    ".cta_burst-lines > svg > path",
-    { drawSVG: "0% 0%" },
-    "<"
-  );
-  ctaFormTrigger.to(
-    ".cta_burst-lines > svg > path",
-    { drawSVG: "100% 100%" },
-    ">-0.3"
-  );
-  ctaFormTrigger.to(
-    ".cta_burst-lines > svg > path",
-    { opacity: 0, duration: 0 },
-    ">-0.05"
-  );
-  ctaFormTrigger.from(
-    ".cta_scribble > svg > path",
-    { drawSVG: "0% 0%" },
-    "<+0.125"
-  );
-  ctaFormTrigger.to(
-    ".cta_scribble > svg > path",
-    { drawSVG: "100% 100%" },
-    ">-0.3"
-  );
-  ctaFormTrigger.to(
-    ".cta_scribble > svg > path",
-    { opacity: 0, duration: 0 },
-    ">-0.02"
-  );
-  //process section animation
+  aboutLoad
+    .from(aboutSplit.lines, { y: 100, scale: 0.8, opacity: 0, stagger: 0.125 })
+    .from(selectors.aboutContent, { opacity: 0, stagger: 0.125 }, "<0.5")
+    .from(selectors.aboutBackground, { scale: 0 }, "<-0.25")
+    .from(
+      [
+        selectors.aboutCardFirst,
+        selectors.aboutCardSecond,
+        selectors.aboutCardThird,
+      ],
+      {
+        x: 2500,
+        rotation: (i) => [100, 60, 130][i],
+        stagger: 0.125,
+      },
+      "<0.5"
+    )
+    .from(selectors.aboutLottie, { scale: 0 }, ">0.125")
+    .from(selectors.aboutLottie2, { scale: 0 }, "<0.25");
+};
 
-  var processLoad = gsap.timeline({
+// CTA Form Animation (Shared for Desktop and Mobile)
+const animateCtaForm = (isMobile = false) => {
+  const ctaFormTrigger = gsap.timeline({
+    scrollTrigger: defaultScrollTrigger(selectors.ctaForm, "-=400 center"),
+  });
+
+  ctaFormTrigger
+    .from(
+      selectors.ctaFormOrange,
+      { color: "#0f2756", duration: 0.125 },
+      "<0.5"
+    )
+    .to(
+      selectors.ctaFormOrange,
+      {
+        scale: 1.1,
+        repeat: 1,
+        yoyo: true,
+        ease: "power2.inOut",
+        duration: 0.3,
+      },
+      "<"
+    );
+  animateSVG(selectors.ctaBurstLines, ctaFormTrigger, "<");
+  animateSVG(selectors.ctaScribble, ctaFormTrigger, "<+0.125");
+
+  if (!isMobile) {
+    const ctaFormLoad = gsap.timeline({
+      scrollTrigger: {
+        ...defaultScrollTrigger(".cta_component"),
+        scrub: true,
+        start: "top top",
+        end: "bottom bottom",
+        pin: ".cta_form-block",
+        pinSpacing: false,
+        fastScrollEnd: true,
+      },
+      defaults: animationDefaults,
+    });
+
+    ctaFormLoad
+      .from(
+        [
+          selectors.ctaHeadingSpan,
+          selectors.ctaFormInput,
+          selectors.ctaFormBottom,
+        ],
+        {
+          y: (i) => [200, 300, 400][i],
+          opacity: 0,
+          stagger: 0.05,
+        },
+        0
+      )
+      .to(
+        [
+          selectors.ctaHeadingSpan,
+          selectors.ctaFormInput,
+          selectors.ctaFormBottom,
+        ],
+        {
+          y: (i) => [-400, -300, -200][i],
+          opacity: 0,
+          stagger: 0.05,
+        },
+        ">+0.25"
+      );
+  }
+};
+
+// Process Section Animation (Desktop)
+const animateProcessDesktop = () => {
+  const processLoad = gsap.timeline({
     scrollTrigger: {
-      trigger: ".process_component",
+      ...defaultScrollTrigger(selectors.processComponent),
       scrub: true,
       start: "top top",
       end: "bottom bottom",
       pin: ".process_content-top",
+      fastScrollEnd: true,
     },
+    defaults: animationDefaults,
   });
 
-  processLoad.from(
-    ".process_heading-span",
-    {
-      y: 200,
-      scale: 0.95,
-      opacity: 0,
-      stagger: 0.05,
-    },
-    0
-  );
-  processLoad.from(
-    ".process_critter-wrapper",
-    { y: 300, scale: 0.9, opacity: 0 },
-    "<"
-  );
-  processLoad.to(
-    ".process_heading-span",
-    { y: -400, scale: 0.95, opacity: 0, duration: 1, stagger: 0.05 },
-    ">"
-  );
-  processLoad.to(".process_critter-wrapper", { y: -200 }, "<+0.15");
-  processLoad.to(
-    ".process_critter",
-    {
-      width: "100dvw",
-      height: "150dvh",
-      borderTopRightRadius: 0,
-      borderTopLeftRadius: 0,
-      border: "transparent",
-    },
-    "<"
-  );
-  processLoad.to(".process_critter-eyes", { opacity: 0, duration: 0.125 }, "<");
-  //cards path animation
-  gsap.set(".process_card-wrapper > .process_card", {
+  processLoad
+    .from(
+      selectors.processHeadingSpan,
+      { y: 200, scale: 0.95, opacity: 0, stagger: 0.05 },
+      0
+    )
+    .from(
+      selectors.processCritterWrapper,
+      { y: 300, scale: 0.9, opacity: 0 },
+      "<"
+    )
+    .to(
+      selectors.processHeadingSpan,
+      { y: -400, scale: 0.95, opacity: 0, stagger: 0.05 },
+      ">"
+    )
+    .to(
+      selectors.processCritterWrapper,
+      {
+        y: -200,
+        onUpdate: () => {
+          gsap.to(selectors.processCritter, {
+            width: "100dvw",
+            height: "150dvh",
+            borderTopRightRadius: 0,
+            borderTopLeftRadius: 0,
+            border: "transparent",
+          });
+        },
+      },
+      "<+0.15"
+    )
+    .to(selectors.processCritterEyes, { opacity: 0, duration: 0.125 }, "<");
+
+  gsap.set(selectors.processCard, {
     xPercent: -50,
     yPercent: -50,
     transformOrigin: "50%, 50%",
   });
   processLoad.to(
-    ".process_card-wrapper > .process_card",
+    selectors.processCard,
     {
       stagger: 0.2,
       ease: "none",
       motionPath: {
-        path: "#process-path",
-        align: "#process-path",
+        path: selectors.processPath,
+        align: selectors.processPath,
         autoRotate: 180,
       },
     },
     ">"
   );
+};
 
-  //pinned service element
-  var servicesPinTrigger = gsap.timeline({
+// Process Section Animation (Mobile)
+const animateProcessMobile = () => {
+  const processMobile = gsap.timeline({
+    scrollTrigger: defaultScrollTrigger(selectors.processComponent),
+    defaults: animationDefaults,
+  });
+
+  processMobile
+    .from(selectors.processHeadingSpan, { y: 100, stagger: 0.1, opacity: 0 }, 0)
+    .from(selectors.processCard, { y: 50, stagger: 0.1, opacity: 0 }, "<0.25");
+};
+
+// Services Section Animation (Desktop)
+const animateServices = () => {
+  gsap.timeline({
     scrollTrigger: {
-      trigger: ".section_services",
+      ...defaultScrollTrigger(selectors.servicesSection),
       start: "top top",
       end: "bottom bottom",
       scrub: true,
-      pin: ".services_content-top-wrapper",
+      pin: selectors.servicesContentTop,
       pinSpacing: false,
+      fastScrollEnd: true,
     },
   });
+};
+
+// Initialize animations based on screen size
+const mm = gsap.matchMedia();
+
+mm.add("(min-width: 991px)", () => {
+  animateHero();
+  animateAbout();
+  animateCtaForm();
+  animateProcessDesktop();
+  animateServices();
 });
 
-//mobile
 mm.add("(max-width: 991px)", () => {
-  var processMobile = gsap.timeline({
-    scrollTrigger: { trigger: ".process_component" },
-  });
-  processMobile.from(
-    ".process_heading-span",
-    { y: 100, stagger: 0.1, opacity: 0 },
-    0
-  );
-  processMobile.from(
-    ".process_card", 
-    {y: 50, stagger: 0.1, opacity: 0}, 
-    "<0.25"
-  )
-  //mobile cta load
-  var ctaFormTrigger = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".cta_form",
-      start: "-=400 center",
-    },
-  });
-  ctaFormTrigger.from(
-    "#cta-form-orange",
-    { color: "#0f2756", duration: 0.125 },
-    "<.5"
-  );
-  ctaFormTrigger.to(
-    "#cta-form-orange",
-    { scale: 1.1, repeat: 1, yoyo: true, ease: "power2.inOut", duration: 0.3 },
-    "<"
-  );
-  ctaFormTrigger.from(
-    ".cta_burst-lines > svg > path",
-    { drawSVG: "0% 0%" },
-    "<"
-  );
-  ctaFormTrigger.to(
-    ".cta_burst-lines > svg > path",
-    { drawSVG: "100% 100%" },
-    ">-0.3"
-  );
-  ctaFormTrigger.to(
-    ".cta_burst-lines > svg > path",
-    { opacity: 0, duration: 0 },
-    ">-0.05"
-  );
-  ctaFormTrigger.from(
-    ".cta_scribble > svg > path",
-    { drawSVG: "0% 0%" },
-    "<+0.125"
-  );
-  ctaFormTrigger.to(
-    ".cta_scribble > svg > path",
-    { drawSVG: "100% 100%" },
-    ">-0.3"
-  );
-  ctaFormTrigger.to(
-    ".cta_scribble > svg > path",
-    { opacity: 0, duration: 0 },
-    ">-0.02"
-  );
+  animateHero();
+  animateAbout();
+  animateCtaForm(true);
+  animateProcessMobile();
 });
 
+// Respect reduced motion preferences
+if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  gsap.globalTimeline.timeScale(0);
+}
+
+// Clean up animations on page unload
+window.addEventListener("beforeunload", () => {
+  ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+});
--- src/pricing.js
+++ src/pricing.js
@@ -1,20 +1,61 @@
-//Pricing stuff
-
+// Set global defaults
 gsap.defaults({
   ease: "power4.out",
   duration: 1,
+  overwrite: "auto", // Prevent animation stacking
 });
 
-// pricing header load in
-var pricingLoad = gsap.timeline({scrollTrigger: {trigger: ".section_pricing"}});
+// Optimize: Use single timeline instance
+const pricingLoad = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".section_pricing",
+    start: "top 80%", // triggers earlier to preload animation
+    toggleActions: "play none none none",
+  },
+});
 
-pricingLoad.from(".pricing_content-top > h1", {y: 100, opacity: 0});
-pricingLoad.from(".pricing_paragraph-wrapper", {y: 150, opacity: 0}, "<+0.25");
-pricingLoad.from(".pricing_content-bottom", {y: 200, opacity: 0}, "<+0.25");
+pricingLoad
+  .from(".pricing_content-top > h1", {
+    y: 80,
+    opacity: 0,
+  })
+  .from(
+    ".pricing_paragraph-wrapper",
+    {
+      y: 100,
+      opacity: 0,
+    },
+    "<+0.2"
+  )
+  .from(
+    ".pricing_content-bottom",
+    {
+      y: 120,
+      opacity: 0,
+    },
+    "<+0.2"
+  );
 
-let mm = gsap.matchMedia();
+// MatchMedia for responsive animation (desktop only)
+gsap.matchMedia().add("(min-width: 991px)", () => {
+  // Debounce ScrollTrigger setup using requestIdleCallback (if available)
+  const setupScroll = () => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".section_services",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: true,
+        pin: ".services_content-top-wrapper",
+        pinSpacing: false,
+        // markers: true // uncomment for debugging
+      },
+    });
+  };
 
-// desktop
-mm.add("(min-width: 991px)", () => {
-   var servicesPinTrigger = gsap.timeline({scrollTrigger: {trigger: ".section_services", start: "top top", end: "bottom bottom", scrub: true, pin: ".services_content-top-wrapper", pinSpacing: false}});
+  if ("requestIdleCallback" in window) {
+    requestIdleCallback(setupScroll);
+  } else {
+    setTimeout(setupScroll, 200); // Fallback
+  }
 });
